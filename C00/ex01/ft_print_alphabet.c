da27e32824bfb74a3e3bf9e136bc23c7989a0fb7
Calleaum <clement1alleaume@gmail.com>
2024-08-12 18:19:02 +0200

    Add files via upload

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calleaum <calleaum@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 15:12:39 by calleaum          #+#    #+#             */
+/*   Updated: 2024/08/09 08:00:36 by calleaum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+/* int	main(void)
+{
+	ft_print_alphabet();
+} */
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calleaum <calleaum@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 15:12:39 by calleaum          #+#    #+#             */
+/*   Updated: 2024/08/09 08:01:37 by calleaum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+/* int	main(void)
+{
+	ft_print_reverse_alphabet();
+} */

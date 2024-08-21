bf8b27a98a18d851bdedd4f0576aadfc0f78796b
Calleaum <clement1alleaume@gmail.com>
2024-08-21 12:25:07 +0200

    Add files via upload

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calleaum <calleaum@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 12:16:21 by calleaum          #+#    #+#             */
+/*   Updated: 2024/08/21 12:22:29 by calleaum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	while (1)
+	{
+		if (ft_is_prime(nb))
+			return (nb);
+		nb++;
+	}
+}
+
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%d", ft_find_next_prime(11));
+// }
